004de237ee50931d2846a9d183d1d66e765e0ba6
Jackie Santana <jackietristansantana@gmail.com>
2021-12-05 15:42:02 -0600

    Update .eslintrc.js

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,19 +1,25 @@
 module.exports = {
   env: {
-    browser: true,
     es2021: true,
+    browser: true,
   },
   extends: [
     'airbnb-base',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 13,
     sourceType: 'module',
   },
-  plugins: [
-    '@typescript-eslint',
-  ],
+  plugins: ['@typescript-eslint', 'prettier', 'import'],
   rules: {
+    'prettier/prettier': 'error',
+    'import/extensions': 'off',
+    'no-console': 'off',
   },
 };
