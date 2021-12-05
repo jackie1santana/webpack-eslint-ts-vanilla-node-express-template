0288169ac01efd50929f9663fd63b0f37f446382
jackie1santana <jackietristansantana@gmail.com>
2021-12-05 11:50:33 -0600

    added static files

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,15 @@
 import express from 'express';
+import path from 'path'
 
 const app = express();
 
 const PORT = 5000;
-app.get('/', (req: any, res: any) => {
-  res.send("dsdk");
-});
+const publicDirectory = path.join(__dirname, "../../client/build");
+app.use(express.static(publicDirectory))
 
+app.get("*", (req, res) => {
+  res.sendFile(publicDirectory);
+});
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
 });
