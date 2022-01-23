9832c3c91ea2c87a1b2107dbf193e5f3f0d9a1da
Jackie Santana <jackietristansantana@gmail.com>
2022-01-22 18:16:49 -0600

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,20 @@
-import express from 'express';
-import path from 'path'
+import express, { Application, Request, Response, NextFunction } from 'express';
+import path from 'path';
 
-const app = express();
+const app: Application = express();
 
 const PORT = 5000;
-const publicDirectory = path.join(__dirname, "../../client/build");
-app.use(express.static(publicDirectory))
+const publicDirectory = path.join(__dirname, '../../client/build');
+app.use(express.static(publicDirectory));
 
-app.get("*", (req, res) => {
-  res.sendFile(publicDirectory);
+// app.get('*', (req, res) => {
+//   res.sendFile(publicDirectory);
+// });
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+  res.send('Hello World!');
 });
+
 app.listen(PORT, () => {
-  console.log(`server running on port ${PORT}`);
+  console.log(`server running on http://localhost:${PORT}`);
 });
